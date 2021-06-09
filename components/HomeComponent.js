995976064d7eecea3ef915fdeb88cb53e4eaa3d8
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-09 14:25:04 -0400

    ScrollView fixes

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { ScrollView, Text, StyleSheet, ImageBackground } from 'react-native';
 
 function Home(props) {
   return (
-    <View>
+    <ScrollView>
       <ImageBackground source={require('../assets/images/cliff-fjord.jpg')} style={styles.image}>
         {/* Title */}
-        <Text>Reise Norge</Text>
+        <Text style={styles.title}>Reise Norge</Text>
       </ImageBackground>
-      <Text>
+      <Text style={styles.text}>
         Löksås ipsum nu tiden genom färdväg strand oss trevnadens, att fram blev kom tiden både när, för i det dunge genom
         vad blivit.
       </Text>
-      <ImageBackground source={require('../assets/images/fjord-ice.jpg')} style={styles.image}>
-        {/* Title */}
-        <Text>Reise Norge</Text>
-      </ImageBackground>
-      <Text>
+      <ImageBackground source={require('../assets/images/fjord-ice.jpg')} style={styles.image} />
+      <Text style={styles.text}>
         Ordningens sorgliga själv hav blivit mjuka sorgliga vad ingalunda samtidigt, precis bland göras är dimma ingalunda i
         år, omfångsrik tiden att gamla blivit vi kanske fram.
       </Text>
-      <ImageBackground source={require('../assets/images/coast.jpg')} style={styles.image}>
-        {/* Title */}
-        <Text>Reise Norge</Text>
-      </ImageBackground>
-    </View>
+      <ImageBackground source={require('../assets/images/coast.jpg')} style={styles.image} />
+
+    </ScrollView>
   );
 }
 
@@ -33,6 +28,12 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 500,
     resizeMode: 'cover',
+  },
+  text: {
+    fontSize: 20,
+  },
+  title: {
+
   },
 });
 
