c0df9b7d916d13d4675a24dee224e04820953099
PatrikBeichner <patrik.beichner@gmail.com>
2021-06-09 14:19:45 -0400

    home screen images and text

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,25 +1,38 @@
-import React from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 
 function Home(props) {
   return (
     <View>
-      <ImageBackground source={require("../assets/images/cliff-fjord.jpg")} style={styles.image}>
+      <ImageBackground source={require('../assets/images/cliff-fjord.jpg')} style={styles.image}>
         {/* Title */}
         <Text>Reise Norge</Text>
       </ImageBackground>
       <Text>
-        
+        Löksås ipsum nu tiden genom färdväg strand oss trevnadens, att fram blev kom tiden både när, för i det dunge genom
+        vad blivit.
       </Text>
+      <ImageBackground source={require('../assets/images/fjord-ice.jpg')} style={styles.image}>
+        {/* Title */}
+        <Text>Reise Norge</Text>
+      </ImageBackground>
+      <Text>
+        Ordningens sorgliga själv hav blivit mjuka sorgliga vad ingalunda samtidigt, precis bland göras är dimma ingalunda i
+        år, omfångsrik tiden att gamla blivit vi kanske fram.
+      </Text>
+      <ImageBackground source={require('../assets/images/coast.jpg')} style={styles.image}>
+        {/* Title */}
+        <Text>Reise Norge</Text>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
-    width: "100%",
+    width: '100%',
     height: 500,
-    resizeMode: "cover",
+    resizeMode: 'cover',
   },
 });
 
